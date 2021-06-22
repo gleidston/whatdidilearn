6aa551d870bde8bde618e0d85051e36279dba3d1
gleidston <king2010-ro@hotmail.com>
2021-06-22 14:01:50 -0300

    ajustando layout aula  21/06 e  corrigindo warning

---
+++ app/src/main/java/com/meupet/whatdidilearn/view/NewItemActivity.kt
@@ -0,0 +1,12 @@
+package com.meupet.whatdidilearn.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meupet.whatdidilearn.R
+
+class NewItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_item)
+    }
+}
