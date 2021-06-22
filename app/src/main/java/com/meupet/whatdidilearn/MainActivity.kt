33ccef60be4bf3769f038a815d7807ac7e394109
gleidston <king2010-ro@hotmail.com>
2021-06-22 16:32:12 -0300

    ajustando layout aula  21/06 adicionando funçoes ao float button

--- app/src/main/java/com/meupet/whatdidilearn/MainActivity.kt
+++ app/src/main/java/com/meupet/whatdidilearn/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.meupet.whatdidilearn
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.meupet.whatdidilearn.data.LearnItemDatabase
 import com.meupet.whatdidilearn.databinding.ActivityMainBinding
 import com.meupet.whatdidilearn.view.LearnItemAdapter
-
+import com.meupet.whatdidilearn.view.NewItemActivity
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,14 +19,16 @@ class MainActivity : AppCompatActivity() {
 
         binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        val tela = findViewById<FloatingActionButton>(R.id.fab)
         val recycler= binding.recyclerView
         val adapter = LearnItemAdapter()
        adapter. learnedItem = LearnItemDatabase.getAll()
         recycler.adapter=adapter
 
-
-
+     tela.setOnClickListener{
+         val intent = Intent(this, NewItemActivity::class.java)
+         startActivities(arrayOf(intent))
+     }
 
     }
 }
