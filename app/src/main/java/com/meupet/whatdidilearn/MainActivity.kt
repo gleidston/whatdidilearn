b7f8ce6dbdd80f2b342f60ab80e297842917be7c
gleidston <king2010-ro@hotmail.com>
2021-06-22 15:24:52 -0300

    ajustando layout aula  21/06 e  corrigindo warning

--- app/src/main/java/com/meupet/whatdidilearn/MainActivity.kt
+++ app/src/main/java/com/meupet/whatdidilearn/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.meupet.whatdidilearn
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.meupet.whatdidilearn.data.LearnItemDatabase
 import com.meupet.whatdidilearn.databinding.ActivityMainBinding
 import com.meupet.whatdidilearn.view.LearnItemAdapter
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val recycler= binding.recyclerView
--- app/src/main/java/com/meupet/whatdidilearn/view/NewItemActivity.kt
+++ app/src/main/java/com/meupet/whatdidilearn/view/NewItemActivity.kt
@@ -1,12 +1,17 @@
 package com.meupet.whatdidilearn.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.meupet.whatdidilearn.MainActivity
 import com.meupet.whatdidilearn.R
 
 class NewItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_item)
+
     }
+
 }
