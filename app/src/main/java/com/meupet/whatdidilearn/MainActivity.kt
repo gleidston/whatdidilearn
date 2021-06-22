0e2b59fb0efce18fffe84767fad217308bdfacf2
gleidston <king2010-ro@hotmail.com>
2021-06-22 16:09:23 -0300

    ajustando layout aula  21/06 e  corrigindo warning parte 2

--- app/src/main/java/com/meupet/whatdidilearn/MainActivity.kt
+++ app/src/main/java/com/meupet/whatdidilearn/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.meupet.whatdidilearn
 
-import android.R
+
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.meupet.whatdidilearn.data.LearnItemDatabase
 import com.meupet.whatdidilearn.databinding.ActivityMainBinding
 import com.meupet.whatdidilearn.view.LearnItemAdapter
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,9 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         binding =ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         val recycler= binding.recyclerView
         val adapter = LearnItemAdapter()
        adapter. learnedItem = LearnItemDatabase.getAll()
